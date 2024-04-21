7c51241c0c6d8c14cdcc7c94ce8845797b80b86a
Владимир <volodya_kuznetsov_04@mail.ru>
2024-04-21 23:49:30 +0500

    Добавьте файлы проекта.

---
+++ MathStat_1/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace MathStat_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button_Calculation = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(24, 52);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(85, 23);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown2.Location = new System.Drawing.Point(24, 110);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(84, 23);
+            this.numericUpDown2.TabIndex = 1;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown3.Location = new System.Drawing.Point(24, 167);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(83, 23);
+            this.numericUpDown3.TabIndex = 2;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            95,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество подбрасываний монеты N";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(20, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество серий экспериментов K";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(20, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Доверительный интервал α";
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineWidth = 0;
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(316, 26);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            this.chart1.Size = new System.Drawing.Size(446, 404);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            title2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            title2.Name = "Title1";
+            this.chart1.Titles.Add(title2);
+            // 
+            // button_Calculation
+            // 
+            this.button_Calculation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Calculation.Location = new System.Drawing.Point(23, 208);
+            this.button_Calculation.Name = "button_Calculation";
+            this.button_Calculation.Size = new System.Drawing.Size(75, 30);
+            this.button_Calculation.TabIndex = 7;
+            this.button_Calculation.Text = "Расчет";
+            this.button_Calculation.UseVisualStyleBackColor = true;
+            this.button_Calculation.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(11, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(266, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Приближенное значение вероятности:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(15, 346);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 9;
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            this.chart2.Location = new System.Drawing.Point(784, 26);
+            this.chart2.Name = "chart2";
+            this.chart2.Size = new System.Drawing.Size(431, 404);
+            this.chart2.TabIndex = 10;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1227, 442);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_Calculation);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Лабораторная 1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button_Calculation;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ MathStat_1/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms.VisualStyles;
+
+namespace MathStat_1
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Очистка графика
+            chart1.Series.Clear();
+            chart2.Series.Clear();
+
+            Function function = new Function();
+
+            int N = ((int)numericUpDown1.Value);
+            int K = ((int)numericUpDown2.Value);
+            float a = ((int)numericUpDown3.Value);
+            a = a / (float)100;
+            
+            //Матрица с частотами
+            List<List<float>> Vo_sum = new List<List<float>>();
+
+            //Проводим серию эксперементов
+            Vo_sum = function.experiment(N, K, Vo_sum);
+
+            //Вывод графика частот
+            for (int i = 0; i < K; i++)
+            {
+                chart1.Series.Add(Name + i);
+                chart1.Series[i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart1.Series[i].Color = Color.Black;
+                for (int j = 1; j <= N; j++)
+                {
+                    chart1.Series[i].Points.AddXY(j, Vo_sum[i][j-1]);
+                }
+            }
+
+            //Матрица с средними значениями
+            List<float> Vo_mean;
+
+            //Рассчитываем значения средней относительной частоты
+            Vo_mean = function.mean(Vo_sum);
+
+            //Строим график средней относительной частоты
+            chart1.Series.Add("average_frequency");
+            chart1.Series["average_frequency"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["average_frequency"].Color = Color.Red;
+            for (int i = 0; i < N; i++)
+            {
+                chart1.Series["average_frequency"].Points.AddXY(i, Vo_mean[i] / (float)K);
+            }
+
+            //Строим график доверительного интервала
+            List < List<float> > Vo_interval = new List<List<float>>();
+            Vo_interval = function.conf_Interval(a, Vo_sum);
+            float down = (1 - a) / 2 * K;
+            float up = K - down - 1;
+            chart1.Series.Add("up");
+            chart1.Series["up"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["up"].Color = Color.Blue;
+            chart1.Series.Add("down");
+            chart1.Series["down"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["down"].Color = Color.Blue;
+            for (int i = 0; i < N; i++)
+            {
+                chart1.Series["up"].Points.AddXY(i, Vo_interval[1][i]);
+                chart1.Series["down"].Points.AddXY(i, Vo_interval[0][i]);
+            } 
+
+            //Приближенное значение вероятности
+            float sum = function.approximate_Value(a, Vo_sum);
+            label5.Text = sum.ToString() + " +- " + ((Vo_interval[0][N-1] - Vo_interval[1][N-1]) / 2).ToString();
+
+            //Вычисляем теоретическую ошибку частоты от количества подбрасывания монеты
+            List<float> exp_error = new List<float>();
+            for (int i = 1; i < N + 1; i++)
+            {
+                exp_error.Add((Vo_interval[0][i-1] - Vo_interval[1][i-1]) / 2);
+            }
+           
+
+            float coef = function.normal_Quantile((1 + a) / 2);
+            List<float> theory_error = new List<float>();
+            
+            for(int i = 1; i < N + 1; i++)
+            {
+                float temp = (float)(coef * Math.Sqrt(((1.0/3.0) * (2.0/3.0) )/ i));
+                theory_error.Add(temp);
+            }
+
+            //Рисуем графики ошибки
+            chart2.Series.Add("theory");
+            chart2.Series["theory"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart2.Series["theory"].Color = Color.Blue;
+            chart2.Series.Add("exp");
+            chart2.Series["exp"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart2.Series["exp"].Color = Color.Red;
+
+            for (int i = 1; i < N; i++)
+            {
+                chart2.Series["theory"].Points.AddXY(i, theory_error[i]);
+                chart2.Series["exp"].Points.AddXY(i, exp_error[i]);
+            }
+
+            //Настройка графика
+            chart1.ChartAreas[0].AxisX.Maximum = N;
+            chart1.ChartAreas[0].AxisX.Minimum = 1;
+            chart1.ChartAreas[0].AxisY.Maximum = 1;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+
+            //Установка логарифмической шкалы на оси X
+            chart1.ChartAreas[0].AxisX.IsLogarithmic = true;
+            chart2.ChartAreas[0].AxisX.IsLogarithmic = true;
+            //Установка базы логарифма
+            chart1.ChartAreas[0].AxisX.LogarithmBase = 10;
+            chart2.ChartAreas[0].AxisX.LogarithmBase = 10;
+
+        }
+    }
+    public class Function
+    {
+        public List<List<float>> experiment(int N, int K, List<List<float>> Vo_sum)
+        {
+            var rand = new Random();
+            //Повторение эксперементов
+            for (int i = 0; i < K; i++)
+            {
+                float V = 0;
+                int count = 0;
+                List<float> Vo = new List<float>();
+                //Повторение бросков
+                for (int j = 0; j < N; j++)
+                {
+
+                    int value = rand.Next(0, 3);
+                    if (value >= 1)
+                    {
+                        V = (float)++count / (float)(j + 1);
+                    }
+                    else
+                    {
+                        V = (float)count / (float)(j + 1);
+                    }
+                    Vo.Add(V);
+                }
+                Vo_sum.Add(Vo);
+            }
+            return Vo_sum;
+        }
+        public List<float> mean(List<List<float>> Vo_sum)
+        {
+            List<float> Vo_mean = new List<float>();
+            int N = Vo_sum[0].Count;
+            int K = Vo_sum.Count;
+            float sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < K; j++)
+                {
+                    sum += Vo_sum[j][i];
+                }
+                Vo_mean.Add(sum);
+                sum = 0;
+            }
+            return Vo_mean;
+        }
+        public List<List<float>> conf_Interval(float a, List<List<float>> Vo_sum)
+        {
+            int N = Vo_sum[0].Count;
+            int K = Vo_sum.Count;
+            float down = (1 - a) / 2 * K;
+            float up = K - down - 1;
+            List<List<float>> Vo_interval = new List<List<float>>();
+            List<float> Vo_up = new List<float>();
+            List<float> Vo_down = new List<float>();
+            Vo_interval.Add(Vo_up);
+            Vo_interval.Add(Vo_down);
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < K - 1; j++)
+                {
+                    for (int c = j + 1; c < K; c++)
+                    {
+                        if (Vo_sum[c][i] > Vo_sum[j][i])
+                        {
+                            var temp = Vo_sum[j][i];
+                            Vo_sum[j][i] = Vo_sum[c][i];
+                            Vo_sum[c][i] = temp;
+                        }
+                    }
+                }
+                Vo_interval[0].Add(Vo_sum[(int)down][i]);
+                Vo_interval[1].Add(Vo_sum[(int)up][i]);
+            }
+            return Vo_interval;
+        }
+        public float approximate_Value(float a, List<List<float>> Vo_sum)
+        {
+            float sum = 0;
+            int N = Vo_sum[0].Count;
+            int K = Vo_sum.Count;
+            float down = (1 - a) / 2 * K;
+            float up = K - down - 1;
+            up = Vo_sum[(int)(up)][N - 1];
+            down = Vo_sum[(int)(down)][N - 1];
+            for (int i = 0; i < K; i++)
+            {
+                sum += Vo_sum[i][N - 1];
+            }
+            sum = sum / (float)K;
+            return sum;
+        }
+        public float normal_Quantile(float p)
+        {
+            return (float)(4.91 * (Math.Pow(p,0.14) - Math.Pow((1 - p),0.14)));
+        }
+    }
+}
